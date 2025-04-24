4fc452a6faa931d66535ace50760725ad84dbfe1
S.M. Mohibullah Bukhari <mohibullahbukhari98@gmail.com>
2025-04-25 00:05:56 +0500

    Remove HomeController and update routing to Expenses
    - Deleted `HomeController.cs` and its associated views (Index and Privacy).
    - Added Swagger support in `Program.cs` for API documentation.
    - Changed default routing to point to `Expenses` controller.
    - Updated navigation in `_Layout.cshtml` to reflect the removal of Home and Privacy links.

--- FinanceApp/Program.cs
+++ FinanceApp/Program.cs
@@ -11,11 +11,19 @@ builder.Services.AddDbContext<FinanceAppContext>(options =>
 
 builder.Services.AddScoped<IExpensesService, ExpensesService>();
 
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment()) {
 	app.UseExceptionHandler("/Home/Error");
+}
+else {
+	app.UseSwagger();
+	app.UseSwaggerUI(); // Optional config: c => c.SwaggerEndpoint(...)
 }
 app.UseStaticFiles();
 
@@ -25,6 +33,8 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+	pattern: "{controller=Expenses}/{action=Index}/{id?}");
+
+app.MapFallbackToController("Index", "Expenses");
 
 app.Run();
